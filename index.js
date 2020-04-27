79ddb8a65e719977bba2323dd7461bef0ba0ddfb
YoungDaggerDee <51313652+YoungDaggerDee@users.noreply.github.com>
2020-04-27 18:50:29 +0200

    Report system, [1.0.1]

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const emoji = require('./emoji.json')
 const Discord = require("discord.js")
+const config = require('./config.json')
 const bot = new Discord.Client()
 const client = new Discord.Client();
 const d = new Date()
 const messages = []
 const report = []
+const botName = config["bot-name"]
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    for(let i=0;i<10;i++){console.log()}
+    console.log("You're running Bot version: ["+config.version+"]")
+    console.log(`Logged in as ${client.user.tag}!`)
 });
   //ON MESSAGE SENT
 client.on('message', msg => {
@@ -19,14 +23,6 @@ client.on('message', msg => {
   //RANDOM EMOJI COMMAND
   if (msg.content === '!emoji'){
       msg.reply(emoji[Math.floor(Math.random()*emoji.length)])
-  }
-  //REPORT COMMAND
-  if (msg.content === '!report'){
-      const user = msg.mentions.users.first();
-      msg.reply("REPORTED")
-      report.push(user)
-      console.log(report)
-      msg.channel.send('reported ' + user)
   }
   //CONSOLE LOG: ALL MESSAGES
   if (msg.content === 'messages'){
@@ -91,8 +87,58 @@ client.on('message', message => {
       message.reply("You didn't mention the user to ban!");
     }
   }
+  //REPORT
+  if (message.content.startsWith('!report')) {
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        message.delete()
+        console.log("REPORTED: ["+user.username+" #"+user.discriminator+"]")
+        report.push(user.username+"#"+user.discriminator)
+
+        const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Report')
+	.setDescription("Uzivatel ["+ user.username+"#"+user.discriminator +"] byl nahlasen")
+	.addFields(
+		{ name:"Dekujeme",value: 'Admin co nejrychleji vyresi tvuj problem' },
+		// { name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	// .addField('Inline field title', 'Some value here', true)
+	// .setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	// .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+  message.channel.send(exampleEmbed);
+
+      } else {
+        message.reply("Hrac nebyl nalezen!");
+      }
+    } else {
+      message.reply("You didn't mention the user to report!");
+    }
+  }
+  if(message.content.startsWith('!list')) {
+    if(report.length == 0){
+      message.channel.send("Zadny hrad nebyl reportovan")
+      return
+    }
+    const exampleEmbed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Report List')
+    .setThumbnail()
+    .addFields(
+        {name:"Reportovani hraci", value: report } 
+    )
+    .setTimestamp()
+  
+  message.channel.send(exampleEmbed);
+    
+  }
 });
 
 
 
-client.login('NjMzOTI5ODcwNjU2NjY3Njc4.XqXPLQ.4TxS52XyR9L2mxmq422jwXIbRJI')
+
+client.login(config.key)
